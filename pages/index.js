e578a62c4d830b337ded447d2b9b8d80f690a53d
reganraja36 <102321874+reganraja36@users.noreply.github.com>
2022-04-17 01:00:23 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,86 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Anthropolo</title>
+        <meta name="Anthropolo" content="Inspired with ❤️ by Anthropolo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <a>Anthropolo</a>
+        </h1>
+        <h2>Building a new internet on Web3</h2>
+
+        <p className={styles.description}>
+          Get started by reading our Whitepaper{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://anthropolo.io" 
+          className={styles.card}
+          >
+            <h2>Polo &rarr;</h2>
+            <p>Web3 Browser</p>
+          </a>
+
+          <a href="https://anthropolo.io" 
+          className={styles.card}
+          >
+            <h2>Replika &rarr;</h2>
+            <p>Web3 Search Engine</p>
+          </a>
+
+          <a href="https://anthropolo.io" 
+          className={styles.card}
+          >
+            <h2>Anthros &rarr;</h2>
+            <p>Web3 DAO</p>
+          </a>
+
+          <a
+            href="https://anthropolo.io"
+            className={styles.card}
+          >
+            <h2>Metagraph &rarr;</h2>
+            <p>Web3 DLT</p>
+          </a>
+
+          <a
+            href="https://anthropolo.io"
+            className={styles.card}
+          >
+            <h2>Myriad &rarr;</h2>
+            <p>Web3 Applications</p>
+          </a>
+
+          <a
+            href="https://anthropolo.io"
+            className={styles.card}
+          >
+            <h2>Rostrum &rarr;</h2>
+            <p>Web3 AI</p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://anthropolo.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Inspired with ❤️ by {' '}
+          <span className={styles.logo}>
+            <img src="/footer.png" alt="Logo" width={80} height={20} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
